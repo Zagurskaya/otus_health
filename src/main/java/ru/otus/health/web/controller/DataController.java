4b69c6d621c3e949ac6f84ddfe2aa6cc25bf3a38
Zagurskaya <natacha.r@mail.ru>
2023-03-28 23:49:22 +0300

    added image zagurskaya/health-image:2.0.0

--- src/main/java/ru/otus/health/web/controller/DataController.java
+++ src/main/java/ru/otus/health/web/controller/DataController.java
@@ -1,17 +1,17 @@
 package ru.otus.health.web.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.otus.health.web.response.Response;
 
 @RestController
 @RequestMapping("health")
 public class DataController {
 
     @GetMapping()
-    public ResponseEntity health() {
-        return new ResponseEntity(HttpStatus.OK);
+    public Response health() {
+
+        return new Response("OK");
     }
 }
